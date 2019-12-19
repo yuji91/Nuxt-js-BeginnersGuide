8ae4aa1cf0c9f6c0b02120f1a778539ab5cf229f
yuji91 <yuji91.1227@gmail.com>
2019-12-19 23:24:51 +0900

    Add page axios_qiita

---
+++ pages/axios_qiita.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>
+      <!-- axios_qiita -->
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    async mounted() {
+      console.log(
+        JSON.stringify(await
+          this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, '')
+      )
+    }
+  }
+</script>
