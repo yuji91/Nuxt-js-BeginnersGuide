7464b0ffa7fcd9e62ab04ed71b2fc6c4c0e29d68
yuji91 <yuji91.1227@gmail.com>
2019-12-19 23:19:39 +0900

    Add dynamic routing contents

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <p>
-      /users/_id.vue
+      User ID: {{ userId }}
     </p>
   </div>
 </template>
+<script>
+  export default {
+    data(){
+      return{
+        userId: this.$route.params.id
+      }
+    }
+  }
+</script>
