50756e93090937d745253381dc9904812cf5edc9
yuji91 <yuji91.1227@gmail.com>
2019-12-22 11:08:49 +0900

    Move qiita api test page

---
+++ pages/qiita/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  async mounted() {
+    console.log(
+      JSON.stringify(await
+      this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, '')
+    )
+  }
+}
+</script>
